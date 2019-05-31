3b29802d8962804c9c619b66f47bf915f9a85773
Sergey Chernolyas <sergey.chernolyas@gmail.com>
2019-05-31 23:48:13 +0300

    show Collection as JSON

--- se.redfield.node.port.orientdb/src/se/redfield/node/port/orientdb/Constants.java
+++ se.redfield.node.port.orientdb/src/se/redfield/node/port/orientdb/Constants.java
@@ -2,6 +2,9 @@ package se.redfield.node.port.orientdb;
 
 import org.knime.core.data.json.JSONCellFactory;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Constants {
 	public static final JSONCellFactory JSON_CELL_FACTORY = new JSONCellFactory();
+	public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 }
